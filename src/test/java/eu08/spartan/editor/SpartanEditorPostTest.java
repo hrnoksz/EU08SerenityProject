662419c782fc3cc3bde0abbd34314d3a7f996d52
hrnoksz <harun1973@yahoo.com>
2022-08-02 20:57:45 +0300

    SpartanEditorPostTest.java added

--- src/test/java/eu08/spartan/editor/SpartanEditorPostTest.java
+++ src/test/java/eu08/spartan/editor/SpartanEditorPostTest.java
@@ -1,7 +1,48 @@
 package eu08.spartan.editor;
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utilities.SpartanNewBase;
+import utilities.SpartanUtil;
 
-public class SpartanEditorPostTest {
+import java.util.Map;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static net.serenitybdd.rest.SerenityRest.given;
 
+@SerenityTest
+public class SpartanEditorPostTest extends SpartanNewBase {
+
+    @DisplayName("Editor should be able to POST")
+    @Test
+    public void postSpartanAsEditor(){
+
+        //When you need deserialize or serialize, you don't need to add
+        //separate dependency, it comes with serenity!!!!!!!!!!!!!!!!!!!!!!!!!
+        //create one spartan using util
+        Map<String, Object> bodyMap = SpartanUtil.getRandomSpartanMap();
+
+        System.out.println("bodyMap = " + bodyMap);
+
+        //send a post request as editor
+        given()
+                .auth().basic("editor", "editor")
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(bodyMap)
+                .log().body()
+                .when()
+                .post("/spartans")
+                .prettyPrint();
+
+    }
 
 }
