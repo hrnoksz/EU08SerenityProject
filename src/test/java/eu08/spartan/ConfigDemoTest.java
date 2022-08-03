fb140e9d9581d70e07697fb09d03c4a6746f81ca
hrnoksz <harun1973@yahoo.com>
2022-08-03 10:04:44 +0300

    serenity.conf added

--- src/test/java/eu08/spartan/ConfigDemoTest.java
+++ src/test/java/eu08/spartan/ConfigDemoTest.java
@@ -1,4 +1,17 @@
 package eu08.spartan;
 
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+
 public class ConfigDemoTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println(ConfigReader.getProperty("serenity.project.name"));
+
+        System.out.println(ConfigReader.getProperty("spartan.editor.username"));
+
+
+    }
 }
